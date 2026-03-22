60a77cea1a5b785ab211bcbe8c4191bdf70b32e1
ricardo2404 <angelricardo1004@hotmail.com>
2026-03-21 21:45:57 -0600

    FEATURE: sparkles: diseño de pagina

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// lib/firebase.ts
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
+  apiKey: "AIzaSyAs3WxviaxYquTfAq4lOEm-TWHwu6n2Nrc", // Aquí pega la clave que viste en la imagen
+  authDomain: "ticket-nft-90f77.firebaseapp.com",
+  projectId: "ticket-nft-90f77",
+  storageBucket: "ticket-nft-90f77.firebasestorage.app",
+  messagingSenderId: "444558724878",
+  appId: "1:444558724878:web:db15054a306d1de2e4515f"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
