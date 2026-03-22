3cd248dfa86738ee8189111deccb642825f023b2
Mariana Velasco <mariana.velasco@cceo.com.mx>
2026-03-21 18:16:07 -0600

    feat :sparkles: change app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import "./globals.css";
 import AuthProvider from "../components/AuthContext";
 import WalletContextProvider from "../components/WalletProvider";
 
+export const metadata = {
+  title: "EventFi",
+  description: "NFT Ticketing en Solana",
+}
+
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
